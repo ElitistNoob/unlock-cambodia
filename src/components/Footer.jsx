a46201b84ade5d5cc6dce470be1325ecc168829d
Kevin Hernot <heavykevin2000@hotmail.com>
2022-11-22 19:35:19 +0700

    reworked footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,47 +1,68 @@
 // Hooks
+import { useState } from "react";
 import { Link } from "react-router-dom";
 // Styles
-import { SeperatorStyled } from "./styles/Seperator.Styled";
-import styles from './styles/Footer.module.scss'
+import styles from "./styles/Footer.module.scss";
 // Assets
+import logo from "./assets/logo-light.png";
 import facebook from "./assets/Facebook-icon.svg";
 import instagram from "./assets/Instagram-icon.svg";
 import twitter from "./assets/Twitter-icon.svg";
 
 export default function Footer() {
+  const [email, setEmail] = useState("");
   const year = new Date().getFullYear();
 
   return (
     <>
-      <section className={styles.footerSection}>
-        <SeperatorStyled style={{marginBottom: "3em"}} />
-        <div className={styles.container}>
+      <section className={styles.footer}>
+        <div className={styles.formContainer}>
+          <div>
+            <h2>
+              Subscribe to Our{" "}
+              <span className="title-highlight">Newsletter</span>
+            </h2>
+            <p>
+              Get the Latest News from Us. Stay up-to-date on Cambodia’s exiting
+              development and be the first to know about travel regulation and
+              the such and such.
+            </p>
+          </div>
+          <form>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                name="email"
+                id="email"
+                onChange={event => setEmail(event.target.value)}
+                value={email}
+                type={"email"}
+                placeholder={"Enter your email"}
+              />
+              <button className="btn primary-btn">Subscribe</button>
+            </div>
+          </form>
+        </div>
+        <div className={styles.info}>
           <div className={styles.col1}>
-            <h4 className={styles.title}>Ask us anything</h4>
-            <p className={styles.sectionSubs}>Unlock Cambodia Office</p>
+            <img src={logo} alt="" />
             <p>
               Salakamreuk Road, Siem Reap, Cambodia <br /> 016 565 496 |
               info@UnlockCambodia.com
             </p>
+            <div className={styles.media}>
+              <Link className="social-media__icons" to="/">
+                <img src={facebook} alt="" />
+              </Link>
+              <Link className="social-media__icons" to="/">
+                <img src={instagram} alt="" />
+              </Link>
+              <Link className="social-media__icons instagram" to="/">
+                <img src={twitter} alt="" />
+              </Link>
+            </div>
           </div>
           <div className={styles.col2}>
-            <h4 className={styles.title}>Subscribe Now</h4>
-            <p>
-              Receive our monthly newsletter to stay up-to-date on the latest
-              travel offers!
-            </p>
-            <form className={styles.form}>
-              <label>
-                <input
-                  type={"email"}
-                  placeholder={"Enter your email"}
-                />
-                <span>Email</span>
-                <button>Send</button>
-              </label>
-            </form>
-          </div>
-          <div className={styles.col3}>
             <Link className="footer-nav__link" to="/Unlock-Cambodia">
               Home
             </Link>
@@ -53,17 +74,6 @@ export default function Footer() {
             </Link>
             <Link className="footer-nav__link" to="/contact-us">
               Contact Us
-            </Link>
-          </div>
-          <div className={styles.media}>
-            <Link className="social-media__icons" to="/">
-              <img src={facebook} alt="" />
-            </Link>
-            <Link className="social-media__icons" to="/">
-              <img src={instagram} alt="" />
-            </Link>
-            <Link className="social-media__icons instagram" to="/">
-              <img src={twitter} alt="" />
             </Link>
           </div>
         </div>
