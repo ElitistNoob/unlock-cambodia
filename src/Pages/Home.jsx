288837c6ab23b72a76878ade08a9e878b9f3fd83
Kevin Hernot <heavykevin2000@hotmail.com>
2022-11-22 19:36:15 +0700

    changed order of components

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@
 import Hero from "../components/Hero";
 import Experiences from "../components/Experiences";
 import Review from "../components/Review";
+import EasyAs from "../components/EasyAs";
 // import PopularTour from "../components/PopularTour";
 import TourList from "../components/TourList";
 
@@ -9,8 +10,9 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <Review />
+      <EasyAs />
       <Experiences />
+      <Review />
       {/* <PopularTour /> */}
       <TourList />
     </main>
