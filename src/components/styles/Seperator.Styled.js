60598a493e43db9ccb1a257699a5637c9b18243a
Kevin Hernot <heavykevin2000@hotmail.com>
2022-11-22 19:43:17 +0700

    added new assets and changed media icons

--- src/components/styles/Seperator.Styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const SeperatorStyled = styled.hr`
-  width: 100%;
-  max-width: 1220px;
-  background-color: rgba(255 255 255 / 0.2);
-  height: 1px;
-  border: none;
-`;
