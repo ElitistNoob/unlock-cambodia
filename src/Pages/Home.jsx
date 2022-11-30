3cdd83012471461405af45f0613551d0ae538a9f
Kevin Hernot <heavykevin2000@hotmail.com>
2022-11-30 21:18:40 +0700

    removed unused code

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,6 @@ import Experiences from "../components/Experiences";
 import Review from "../components/Review";
 import EasyAs from "../components/EasyAs";
 import Tailored from "../components/TailoredTours";
-// import PopularTour from "../components/PopularTour";
 import TourList from "../components/TourList";
 
 export default function Home() {
