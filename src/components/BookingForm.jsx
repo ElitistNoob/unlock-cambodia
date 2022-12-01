2628263f0673fd345db644b5686553249e337e0e
Kevin Hernot <heavykevin2000@hotmail.com>
2022-12-01 10:54:15 +0700

    fixed booking redirect link

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -129,11 +129,7 @@ export default function BookingForm({ thisTour, closeModal }) {
           value={`New submission for ${tourForm.tour}`}
         ></input>
         {/* Redirect to custom page */}
-        <input
-          type="hidden"
-          name="_next"
-          value="http://localhost:3000/form-submit"
-        ></input>
+        <input type="hidden" name="_next" value="/form-submit"></input>
         {/* Sends an automated response to sender*/}
         <input
           type="hidden"
