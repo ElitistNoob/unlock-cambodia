121ceeb1900951ebc2e4ef5e91d13faad4c25a85
Kevin Hernot <heavykevin2000@hotmail.com>
2022-12-02 16:04:42 +0700

    Change to grid-template-areas

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -32,22 +32,24 @@ export default function AboutUs() {
         <h1>
           Our <span className="span title-highlight">Story</span>
         </h1>
-        <p>
-          Unlock Cambodia has been arranging trips, tours and adventures since
-          2010. The starting point in all of this was when English and Czech
-          couple Peter and Eva decided to see what hidden gems Cambodia had to
-          offer; after the average tourist at the time spend only 3 days in
-          Cambodia. At the time seeing Angkor what was pretty much the only site
-          that tourists wanted to see before moving on. But there had to be more
-          to Cambodia that just the temples right!
-        </p>
-        <p>
-          This is when we teamed up with local Cambodian Synak (Brian) Ork; with
-          the aim of Unlocking Cambodia for everyone that wants to discover more
-          about the country’s secrets and hidden gems.
-        </p>
+        <img className="content" src={teamPhoto} alt="Unlock Cambodia Team" />
+        <div>
+          <p>
+            Unlock Cambodia has been arranging trips, tours and adventures since
+            2010. The starting point in all of this was when English and Czech
+            couple Peter and Eva decided to see what hidden gems Cambodia had to
+            offer; after the average tourist at the time spend only 3 days in
+            Cambodia. At the time seeing Angkor what was pretty much the only
+            site that tourists wanted to see before moving on. But there had to
+            be more to Cambodia that just the temples right!
+          </p>
+          <p>
+            This is when we teamed up with local Cambodian Synak (Brian) Ork;
+            with the aim of Unlocking Cambodia for everyone that wants to
+            discover more about the country’s secrets and hidden gems.
+          </p>
+        </div>
       </div>
-      <img className="content" src={teamPhoto} alt="Unlock Cambodia Team" />
     </main>
   );
 }
